90cf520b0da6457db2b6b7b8d06d11367898c226
AdityaDheer <ad4961@nyu.edu>
2022-10-07 14:01:39 -0400

    added level2 and dynamically spawning enemies

--- Assets/Code/KeyDestroyDoor.cs
+++ Assets/Code/KeyDestroyDoor.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class KeyDestroyDoor : MonoBehaviour
 {
     public GameObject door;
+    public float posx;
+    public float posz;
+
+    public GameObject enemy;
     
     private void OnCollisionEnter(Collision other) {
         if (other.gameObject.CompareTag("Player")) {
             print("This ran");
         Destroy(door);
         Destroy(gameObject);
+        Vector3 position = new Vector3(posx, 1, posz);
+        GameObject newEnemy = Instantiate(enemy, position, new Quaternion(0,0,0,1));
         }
     }
 }
