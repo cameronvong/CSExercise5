61247f23637daf0f87f762d1c1142909bdcfc3c1
AdityaDheer <ad4961@nyu.edu>
2022-10-07 03:47:49 -0400

    fixed the camera follow code

--- Assets/Code/CameraFollow.cs
+++ Assets/Code/CameraFollow.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
+    Transform playerTrans;
 
-        public Transform player;
+    Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTrans = GameObject.FindGameObjectWithTag("Player").transform;
+        offset = transform.position - playerTrans.position;
+    }
 
+    // Update is called once per frame
     void LateUpdate()
     {
-        transform.position = player.transform.position + new Vector3(0, 1, -5);
-
+        transform.position = playerTrans.position + offset;
     }
 }
